4ab3c58c5d358835babced09b143c77dbf466889
itayeylath <itay.ey@gmail.com>
2023-02-03 09:52:54 +0200

    Add to backend django-cors-headers and Configuration to allow requests

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -27,10 +27,13 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'corsheaders',
     'base.apps.BaseConfig',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -114,4 +117,4 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Allow everyone to requesting Django.
-CORS_ALLOW_ALL_ORIGINS=True
+CORS_ALLOW_ALL_ORIGINS = True
