b50312e6656ff71d7fd06c86e5c8b0423c69c548
itayeylath <itay.ey@gmail.com>
2023-02-03 14:35:29 +0200

    Add react-spinners - load component

--- frontend/src/components/graph-component.tsx
+++ frontend/src/components/graph-component.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { Line } from "react-chartjs-2";
+import ClipLoader from "react-spinners/ClipLoader";
 import {
   Chart as ChartJS,
   Title,
@@ -45,19 +46,26 @@ const defultGraphData: graphObj = {
 };
 // todo: type for props
 const Graph = (props: GraphProps) => {
-  const [graphLabels, setGraphLabels] = useState<GraphLabels[] | []>([]);
   const [graphData, setGraphData] = useState<GraphData[] | []>([]);
+  const [newData, setNewdata] = useState<GraphData[] | []>([]);
   const [isGraphLoad, setIsGraphLoad] = useState<boolean>(false);
   const [graphObj, setGraphObj] = useState<graphObj>(defultGraphData);
   const [intervalId, setIntervalId] = useState<NodeJS.Timer>();
   const [isStartSample, setIsStartSample] = useState<boolean>(false);
   const [isPauseSample, setIsPauseSample] = useState<boolean>(true);
   const [isStopSample, setIsStopSample] = useState<boolean>(true);
+  const [loading, setLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    setLoading(true)
+    setTimeout( () => {
+        setLoading(false)
+    },500)
+  }, []);
 
   // Get all semples from DB ONLY at the first time of lodeing the page.
   useEffect(() => {
     axiosGetAllsamples("facebook").then((result) => {
-      setGraphLabels([...result]);
       setGraphData([...result]);
       const data = getSortArr(result);
       const labels = getRoundArr(data, props.decimalRound);
@@ -96,8 +104,8 @@ const Graph = (props: GraphProps) => {
 
     let sampleInterval = setInterval(() => {
       axiosGetsample(props.webName).then((result) => {
-        setGraphLabels((oldArr: any) => [...oldArr, result]);
         setGraphData((oldArr: any) => [...oldArr, result]);
+        setNewdata((oldArr: any) => [...oldArr, result]);
         setIsGraphLoad((prev: any) => !prev);
       });
     }, props.sampleRate);
@@ -108,10 +116,13 @@ const Graph = (props: GraphProps) => {
     console.log("Start");
   };
 
+
   const handelButtonPause = () => {
     clearInterval(intervalId);
+
     setIsStartSample(false);
     setIsPauseSample(true);
+    setIsStopSample(false);
 
     setIsGraphLoad((prev: any) => !prev);
     console.log("Pause");
@@ -119,23 +130,44 @@ const Graph = (props: GraphProps) => {
 
   const handelButtonStop = () => {
     setIsStartSample(false);
+    setIsPauseSample(true);
     setIsStopSample(true);
     clearInterval(intervalId);
     setIsGraphLoad((prev: any) => !prev);
     console.log("Stop");
-    axiosPostSamples({ arr: graphData }).then((result) => {
-      //todo: load ui until it ok
+    setLoading(true)
+    axiosPostSamples({ "arr": newData }).then((result) => {
+        setLoading(false)
+      console.log("saved!")
     });
   };
 
   return (
     <div>
       <div style={{ width: "500px", height: "200px" }}>
+        {
+        loading ?
+        
+        <ClipLoader
+        color={"#36d7b7"}
+        loading={loading}
+        size={150}
+        cssOverride={{}}
+      />
+      :
+
         <Line data={graphObj}>Fiest test</Line>
+        }
       </div>
-      <button disabled={isStartSample} onClick={handelButtonStart}>START</button>
-      <button disabled={isPauseSample} onClick={handelButtonPause}>PAUSE</button>
-      <button disabled={isStopSample} onClick={handelButtonStop}>STOP</button>
+      <button disabled={isStartSample} onClick={handelButtonStart}>
+        START
+      </button>
+      <button disabled={isPauseSample} onClick={handelButtonPause}>
+        PAUSE
+      </button>
+      <button disabled={isStopSample} onClick={handelButtonStop}>
+        STOP
+      </button>
     </div>
   );
 };
